91aa53c94fff1f2da5e64917f7f383e27e80cec4
BLYTH3 <ST4R13UCK@gmail.com>
2013-02-06 13:06:04 +0700

    Set Controller in MainActivity

--- src/th/co/samsung/scloud/MainActivity.java
+++ src/th/co/samsung/scloud/MainActivity.java
@@ -1,15 +1,43 @@
 package th.co.samsung.scloud;
 
+import th.co.samsung.scloud.Documents.OnDocumentsChangeListener;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnDocumentsChangeListener {
+
+	private Documents mDocuments;
+	private Document mDocument;
+	private ListView mListView;
+	private DocumentListAdapter mAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mDocuments = new Documents();
+
+		// bootstrap
+		mDocument = new Document("Title", "Modify");
+
+		mDocuments.insert(mDocument);
+		mListView = (ListView) findViewById(R.id.listView);
+		mAdapter = new DocumentListAdapter(this) {
+
+			@Override
+			public Object getItem(int position) {
+				return mDocuments.get(position);
+			}
+
+			@Override
+			public int getCount() {
+				return mDocuments.size();
+			}
+		};
+		mListView.setAdapter(mAdapter);
+		mDocuments.setOnDocumentsChangeListener(this);
 	}
 
 	@Override
@@ -17,6 +45,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+
+	@Override
+	public void onDucumentChange(Documents documents) {
+		mAdapter.notifyDataSetChanged();
 	}
 
 }
